6c5bbfa487686c8f1dd8251fc9ef73dd051baf78
sheenie <sheenie@student.chalmers.se>
2024-04-19 18:49:01 +0200

    Integrate MQTT into cue setup to play sound on the webpage and highlight the selected answer from the Wio terminal

--- guess-the-sound-game/src/components/SoundGame.vue
+++ guess-the-sound-game/src/components/SoundGame.vue
@@ -17,13 +17,18 @@
 <script>
   import { ref } from 'vue';
   import { animals } from '@/assets/animal.js'; 
+  import * as mqtt from 'mqtt';
   
   export default {
   name: 'SoundGame',
   setup() {
+    const mqttClient = ref(null);
+    const mqttTopic1 = 'pll/game-terminal/sound-game/replay-sound';
+    const mqttTopic2 = 'pll/game-terminal/sound-game/answer';   
     const audioPlayer = ref(null);
     var arrDisplayedAnimals = [];
     const size = 4; // number of animals displayed
+
     function generateIndexArray(size) {
         const uniqueIndexes = new Set(); // no repeats
 
@@ -41,24 +46,43 @@
     const selectedIndex = Math.floor(Math.random() * size); // 0-3 
     const selectedAnimal = ref(arrDisplayedAnimals[selectedIndex]);
     const selectedSound = ref(selectedAnimal.value.sound);
-    
-    const displayedAnimals = ref(arrDisplayedAnimals); // 4 animals being displayed
-
-    // const test = ref(selectedAnimal);
+    const displayedAnimals = ref(arrDisplayedAnimals); // 4 animals being displayed 
 
     const playSound = () => {
       audioPlayer.value.play();
     };
 
-    const checkAnswer = (animal) => {
-      if (animal.name === selectedAnimal.value.name) {
+    const checkAnswer = (animalName) => {
+      if (animalName === selectedAnimal.value.name) {
         alert("Correct!");
       } else {
         alert("Try again!");
       }
     };
 
-    return { displayedAnimals, selectedSound, audioPlayer, playSound, checkAnswer };
+    const initialiseMQTT = () => {
+      mqttClient.value = mqtt.connect('ws://broker.hivemq.com:1883'); // is the the correct broker's address?
+      
+      mqttClient.value.on('connect', () => {
+        console.log('Connected to MQTT broker');
+        mqttClient.value.subscribe([mqttTopic1, mqttTopic2]);
+      });
+
+      mqttClient.value.on('message', (mqttTopic, message) => {
+        console.log(`Received message on ${topic}: ${message.toString()}`);
+        if (mqttTopic === mqttTopic1 && message.toString() === 'replay') {
+          playSound();
+          console.log(`Received message: ${message.toString()}`);
+        } else if (mqttTopic === mqttTopic2) {
+          console.log(`Received message: ${message.toString()}`);
+          checkAnswer(message.toString()); // message is name of selected animal from wio terminal
+        }
+      });
+    };
+
+    initialiseMQTT();
+
+    return { displayedAnimals, selectedSound, audioPlayer, playSound, checkAnswer, mqttClient };
   }
 };
 </script>
