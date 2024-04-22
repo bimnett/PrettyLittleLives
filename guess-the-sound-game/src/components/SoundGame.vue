b5d0e97b720cf4f7443d9a8643849b054fd7b92c
sheenie <sheenie@student.chalmers.se>
2024-04-22 18:02:58 +0200

    #8 Set up mock broker to test subscribing and publishing messages to subscribed  topics.

--- guess-the-sound-game/src/components/SoundGame.vue
+++ guess-the-sound-game/src/components/SoundGame.vue
@@ -2,11 +2,13 @@
   <div class="game-container">
     <div class="header">
       <h1 class="title">Guess the Animal!</h1>
-      <button class="sound-button" @click="playSound">Play Sound</button>
+      <!-- <button class="sound-button" @click="playSound">Play Sound</button> -->
+      <button class="sound-button" @click="testPublishPlay">Test Publish</button> <!-- New button for testing -->
       <audio :src="selectedSound" ref="audioPlayer"></audio>
     </div>
     <div class="images-grid">
-        <div class="image-box" v-for="(animal, index) in displayedAnimals" :key="animal.name" @click="checkAnswer(animal)">
+        <!-- <div class="image-box" v-for="(animal, index) in displayedAnimals" :key="animal.name" @click="checkAnswer(animal)"> -->
+        <div class="image-box" v-for="(animal, index) in displayedAnimals" :key="animal.name" @click="testPublishCheckAnswer(animal)">
           <img :src="animal.image" :alt="animal.name" class="animal-image">
           <p class="animal-name">{{ animal.name }}</p>
         </div>
@@ -18,13 +20,18 @@
   import { ref } from 'vue';
   import { animals } from '@/assets/animal.js'; 
   import * as mqtt from 'mqtt';
+  import MockBroker from '@/utils/MockBroker';
   
   export default {
   name: 'SoundGame',
   setup() {
-    const mqttClient = ref(null);
-    const mqttTopic1 = 'pll/game-terminal/sound-game/replay-sound';
-    const mqttTopic2 = 'pll/game-terminal/sound-game/answer';   
+    // const mqttClient = ref(null);
+    // const mqttTopic1 = 'pll/game-terminal/sound-game/replay-sound';
+    // const mqttTopic2 = 'pll/game-terminal/sound-game/answer';   
+    const broker = new MockBroker();
+    const mockTopic1 = 'game/playSound';
+    const mockTopic2 = 'game/answer';       
+
     const audioPlayer = ref(null);
     var arrDisplayedAnimals = [];
     const size = 4; // number of animals displayed
@@ -52,57 +59,88 @@
       audioPlayer.value.play();
     };
 
-    const checkAnswer = (animalName) => {
-      if (animalName === selectedAnimal.value.name) {
+    const checkAnswer = (animal) => {
+      if (animal.name === selectedAnimal.value.name) {
+    // const checkAnswer = (animalName) => {
+    //   if (animalName === selectedAnimal.value.name) {
         alert("Correct!");
       } else {
         alert("Try again!");
       }
     };
 
-    const initialiseMQTT = () => {
-      mqttClient.value = mqtt.connect('ws://broker.hivemq.com:1883'); // is the the correct broker's address?
+    // const initialiseMQTT = () => {
+    //   mqttClient.value = mqtt.connect('ws://broker.hivemq.com:1883'); // is the the correct broker's address?
       
-      mqttClient.value.on('connect', () => {
-        console.log('Connected to MQTT broker');
-        mqttClient.value.subscribe([mqttTopic1, mqttTopic2]);
-      });
+    //   mqttClient.value.on('connect', () => {
+    //     console.log('Connected to MQTT broker');
+    //     mqttClient.value.subscribe([mqttTopic1, mqttTopic2]);
+    //   });
 
-      mqttClient.value.on('message', (mqttTopic, message) => {
-        console.log(`Received message on ${topic}: ${message.toString()}`);
-        if (mqttTopic === mqttTopic1 && message.toString() === 'replay') {
-          playSound();
-          console.log(`Received message: ${message.toString()}`);
-        } else if (mqttTopic === mqttTopic2) {
-          console.log(`Received message: ${message.toString()}`);
-          checkAnswer(message.toString()); // message is name of selected animal from wio terminal
-        }
-      });
-    };
+    //   mqttClient.value.on('message', (mqttTopic, message) => {
+    //     console.log(`Received message on ${topic}: ${message.toString()}`);
+    //     if (mqttTopic === mqttTopic1 && message.toString() === 'replay') {
+    //       playSound();
+    //       console.log(`Received message: ${message.toString()}`);
+    //     } else if (mqttTopic === mqttTopic2) {
+    //       console.log(`Received message: ${message.toString()}`);
+    //       checkAnswer(message.toString()); // message is name of selected animal from wio terminal
+    //     }
+    //   });
+    // };
 
-    initialiseMQTT();
+    // initialiseMQTT();
 
-    return { displayedAnimals, selectedSound, audioPlayer, playSound, checkAnswer, mqttClient };
+    broker.subscribe(mockTopic1, (topic, message) => {
+      console.log(`Received message on ${topic}: ${message}`);
+      if (topic === mockTopic1 && message === 'play') {
+        playSound();
+      }
+    });
+
+    broker.subscribe(mockTopic2, (topic, message) => {
+      console.log(`Received message on ${topic}: ${message}`);
+      if (topic === mockTopic2) {
+        checkAnswer(message); // message is the name of the selected animal from the Wio Terminal
+      }
+    });
+
+    function testPublishPlay() {
+      broker.publish(mockTopic1, 'play');
+    }
+
+    function testPublishCheckAnswer(animal) {
+      const animalName = animal.name;
+      broker.publish(mockTopic2, animalName);
+    }    
+
+    return { displayedAnimals, selectedSound, audioPlayer, playSound, checkAnswer, testPublishPlay, testPublishCheckAnswer }; // mqttClient
   }
 };
+
 </script>
 
 <style>
   .game-container {
+    margin: auto;
+    justify-content: left;
     text-align: center;
     background-color: #FCF3CF;
+    min-width: 90vh;
+    height: auto;
+    padding: 0;
   }
 
   .header {
     display: flex;
     justify-content: space-between; 
-    margin-bottom: 10px;
+    margin: 10px;
   }
 
   .title {
-    font-size: 4rem;
+    font-size: 3rem;
+    line-height: 1.8;
     margin: 0;
-    flex-grow: 1;
     color: #7D6608;
     font-family:'Impact';    
   }
@@ -129,8 +167,8 @@
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-template-rows: 1fr 1fr;
-    width: 100vw;
-    height: 100vw;
+    width: 100%;
+    height: auto;
     gap: 0px;
   }
 
