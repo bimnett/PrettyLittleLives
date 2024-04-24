6547ebe504d13215c50eed598abbd7eb98b205ab
Fatma Gichaba <fatma@MacBook-Air-som-tillhor-Fatma.local>
2024-04-24 12:07:31 +0200

    #1 Fix decibel reading
     Co-authored with Ling

--- SoundLevel.c
+++ SoundLevel.c
@@ -1,27 +1,36 @@
-#include <Arduino.h> 
 
+const int sampleWindow = 50; // Time frame where millis function will run. 
+unsigned int soundSample; // sound sample collected within the sampleWindow
 
-void setup() {
-  Serial.begin(9600);
+void setup()
+{
+    Serial.begin(9600);
 }
 
-void loop() {
-  int sensorValue = analogRead(A0);
-  float decibelValue = analogToDecibel(sensorValue);
+void loop()
+{
+  unsigned long startMillis = millis(); // Unsigned = always positive values. The milliseconds will always be positive.  
+  float peakToPeak = 0; 
+  unsigned int signalMax = 0; 
+  unsigned int signalMin = 1023; 
 
-  Serial.print("Analog value: ");
-  Serial.println(sensorValue);
+  while (millis() - startMillis < sampleWindow){ 
+    sample = analogRead(0);
+    if(sample < 1023){
+      if( sample > signalMax){
+        signalMax = sample; 
+      }
+      else if (sample < signalMin){
+        signalMin = sample; 
+      }
+    }
+  }
+  
+  peakToPeak = signalMax - signalMin; 
+  // Mapping from anlog to decibel
+  int db = map (peakToPeak, 20, 900, 49.5, 90); 
 
-  Serial.print("Decibel value: ");
-  Serial.println(decibelValue);
-
-  delay(1000);
-}
-
-float analogToDecibel(float analogValue) {
-  float referenceVoltage = 5.0;
-  float currentVoltage = analogValue * (referenceVoltage / 1023.0);
-  float decibelValue = 20 * log10(currentVoltage / referenceVoltage);
-  return decibelValue;
+    Serial.println(db);
+    delay(1000);
 }
 
