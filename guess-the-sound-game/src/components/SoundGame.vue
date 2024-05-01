3b2a1e3c39521b14e7d050acb2b267295ac2f7fd
sheenie <sheenie@student.chalmers.se>
2024-05-01 18:39:51 +0200

    #8 Publish messages of animal choice options and correct/incorect answer to MQTT broker. Remove mock broker.

--- guess-the-sound-game/src/components/SoundGame.vue
+++ guess-the-sound-game/src/components/SoundGame.vue
@@ -2,13 +2,11 @@
   <div class="game-container">
     <div class="header">
       <h1 class="title">Guess the Animal!</h1>
-      <!-- <button class="sound-button" @click="playSound">Play Sound</button> -->
-      <button class="sound-button" @click="testPublishPlay">Test Publish</button> <!-- New button for testing -->
+      <button class="sound-button" @click="playSound">Play Sound</button>
       <audio :src="selectedSound" ref="audioPlayer"></audio>
     </div>
     <div class="images-grid">
-        <!-- <div class="image-box" v-for="(animal, index) in displayedAnimals" :key="animal.name" @click="checkAnswer(animal)"> -->
-        <div class="image-box" v-for="(animal, index) in displayedAnimals" :key="animal.name" @click="testPublishCheckAnswer(animal)">
+        <div class="image-box" v-for="(animal, index) in displayedAnimals" :key="animal.name" @click="checkAnswer(animal)">
           <img :src="animal.image" :alt="animal.name" class="animal-image">
           <p class="animal-name">{{ animal.name }}</p>
         </div>
@@ -17,104 +15,103 @@
 </template>
   
 <script>
-  import { ref } from 'vue';
+  import { ref, onMounted } from 'vue';
   import { animals } from '@/assets/animal.js'; 
-  import * as mqtt from 'mqtt';
-  import MockBroker from '@/utils/MockBroker';
+  import mqtt from 'mqtt';
+  import { HOST } from "../credentials";
   
   export default {
-  name: 'SoundGame',
-  setup() {
-    // const mqttClient = ref(null);
-    // const mqttTopic1 = 'pll/game-terminal/sound-game/replay-sound';
-    // const mqttTopic2 = 'pll/game-terminal/sound-game/answer';   
-    const broker = new MockBroker();
-    const mockTopic1 = 'game/playSound';
-    const mockTopic2 = 'game/answer';       
+    name: 'SoundGame',
+    setup() {
+      const mqttClient = ref(null);
+      const mqttTopicReplay = 'pll/game-terminal/sound-game/replay-sound';
+      const mqttTopicAnswer = 'pll/game-terminal/sound-game/answer';
+      const mqttTopicOptions = 'pll/game-terminal/sound-game/options';
 
-    const audioPlayer = ref(null);
-    var arrDisplayedAnimals = [];
-    const size = 4; // number of animals displayed
+      const audioPlayer = ref(null);
+      var arrDisplayedAnimals = [];
+      const size = 4; // number of animals displayed
 
-    function generateIndexArray(size) {
-        const uniqueIndexes = new Set(); // no repeats
+      function generateIndexArray(size) {
+          const uniqueIndexes = new Set(); // no repeats
 
-        while (uniqueIndexes.size < size) {
-            const randomNumber = Math.floor(Math.random() * 20); // number between 0-19
-            uniqueIndexes.add(randomNumber);
-        }
-        return Array.from(uniqueIndexes);
-    }
-    const arrIndexDisplayedAnimals = generateIndexArray(size); // array of displayed animals
-    for (var i = 0, t = size; i < t; i++) {
-        arrDisplayedAnimals.push(animals[arrIndexDisplayedAnimals[i]])
-    }
-
-    const selectedIndex = Math.floor(Math.random() * size); // 0-3 
-    const selectedAnimal = ref(arrDisplayedAnimals[selectedIndex]);
-    const selectedSound = ref(selectedAnimal.value.sound);
-    const displayedAnimals = ref(arrDisplayedAnimals); // 4 animals being displayed 
-
-    const playSound = () => {
-      audioPlayer.value.play();
-    };
-
-    const checkAnswer = (animal) => {
-      if (animal.name === selectedAnimal.value.name) {
-    // const checkAnswer = (animalName) => {
-    //   if (animalName === selectedAnimal.value.name) {
-        alert("Correct!");
-      } else {
-        alert("Try again!");
+          while (uniqueIndexes.size < size) {
+              const randomNumber = Math.floor(Math.random() * 20); // number between 0-19
+              uniqueIndexes.add(randomNumber);
+          }
+          return Array.from(uniqueIndexes);
       }
-    };
+      const arrIndexDisplayedAnimals = generateIndexArray(size); // array of displayed animals
+      for (var i = 0, t = size; i < t; i++) {
+          arrDisplayedAnimals.push(animals[arrIndexDisplayedAnimals[i]])
+      }
 
-    // const initialiseMQTT = () => {
-    //   mqttClient.value = mqtt.connect('ws://broker.hivemq.com:1883'); // is the the correct broker's address?
+      const selectedIndex = Math.floor(Math.random() * size); // 0-3 
+      const selectedAnimal = ref(arrDisplayedAnimals[selectedIndex]);
+      const selectedSound = ref(selectedAnimal.value.sound);
+      const displayedAnimals = ref(arrDisplayedAnimals); // 4 animals being displayed 
+
+      const playSound = () => {
+        audioPlayer.value.play();
+      };
+
       
-    //   mqttClient.value.on('connect', () => {
-    //     console.log('Connected to MQTT broker');
-    //     mqttClient.value.subscribe([mqttTopic1, mqttTopic2]);
-    //   });
+      const checkAnswer = (animalName) => {
+        if (animalName === selectedAnimal.value.name) {
+          alert("Correct!");
+          mqttClient.value.publish(mqttTopicAnswer, "correct");
+        } else {
+          alert("Try again!");
+          mqttClient.value.publish(mqttTopicAnswer, "incorrect");
+        }
+      };
 
-    //   mqttClient.value.on('message', (mqttTopic, message) => {
-    //     console.log(`Received message on ${topic}: ${message.toString()}`);
-    //     if (mqttTopic === mqttTopic1 && message.toString() === 'replay') {
-    //       playSound();
-    //       console.log(`Received message: ${message.toString()}`);
-    //     } else if (mqttTopic === mqttTopic2) {
-    //       console.log(`Received message: ${message.toString()}`);
-    //       checkAnswer(message.toString()); // message is name of selected animal from wio terminal
-    //     }
-    //   });
-    // };
+      const initialiseMQTT = () => {
 
-    // initialiseMQTT();
+        // const options = {
+        //   connectTimeout: 4000, // milliseconds
+        //   username: 'test1',
+        //   password: 'testing1.A',
+        // };
 
-    broker.subscribe(mockTopic1, (topic, message) => {
-      console.log(`Received message on ${topic}: ${message}`);
-      if (topic === mockTopic1 && message === 'play') {
-        playSound();
-      }
+        mqttClient.value = mqtt.connect(HOST);
+
+        // message published to mqtt
+        const publishAnimalNames = () => {
+          const animalNames = arrDisplayedAnimals.map(animal => animal.name).join(',') + ','; // example: 'Monkey,Cow,Pig,Dog,''
+          mqttClient.value.publish(mqttTopicOptions, animalNames);
+        };        
+        
+        mqttClient.value.on('connect', () => {
+          console.log('Connected to local MQTT broker');
+          mqttClient.value.subscribe([mqttTopicReplay, mqttTopicAnswer]);
+          publishAnimalNames(); // publishes a string of animal names to the broker
+        });
+
+        // messages subscribed to mqtt
+        mqttClient.value.on('message', (mqttTopic, message) => {
+          console.log(`Received message on ${mqttTopic}: ${message.toString()}`);
+          if (mqttTopic === mqttTopicReplay && message.toString() === 'replay') {
+            playSound();
+            console.log(`Received message: ${message.toString()}`);
+          } else if (mqttTopic === mqttTopicAnswer) {
+            console.log(`Received message: ${message.toString()}`);
+            checkAnswer(message.toString()); // message is name of selected animal from wio terminal
+          }
+        });
+
+        mqttClient.value.on('error', (error) => {
+          console.error('Connection failed:', error);
+          mqttClient.value.end();
+        });
+      
+    };
+
+    onMounted(() => {
+      initialiseMQTT();
     });
 
-    broker.subscribe(mockTopic2, (topic, message) => {
-      console.log(`Received message on ${topic}: ${message}`);
-      if (topic === mockTopic2) {
-        checkAnswer(message); // message is the name of the selected animal from the Wio Terminal
-      }
-    });
-
-    function testPublishPlay() {
-      broker.publish(mockTopic1, 'play');
-    }
-
-    function testPublishCheckAnswer(animal) {
-      const animalName = animal.name;
-      broker.publish(mockTopic2, animalName);
-    }    
-
-    return { displayedAnimals, selectedSound, audioPlayer, playSound, checkAnswer, testPublishPlay, testPublishCheckAnswer }; // mqttClient
+    return { displayedAnimals, selectedSound, audioPlayer, playSound, checkAnswer, mqttClient };
   }
 };
 
