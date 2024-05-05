7442005f866931fd9bb0b7a2f4d6fcda01df78c9
Bimnet <gustesbi@student.gu.se>
2024-05-05 10:42:50 +0200

    #14 Change path for web app

--- ci-compile.sh
+++ ci-compile.sh
@@ -1,5 +1,5 @@
 # Change directory to src folder of web app
-cd /builds/bimnet/pll-ci-test/Web/pll-web-app/src
+cd /builds/courses/dit113/2024/group-15/pll/Web/pll-web-app/src
 
 # Add mqtt credentials
 echo "export const HOST = '$WEB_MQTT_HOST';" > credentials.js
