955f236749579eee20408058b6b9b1d4663c19bb
Bimnet <gustesbi@student.gu.se>
2024-05-06 20:03:47 +0200

    #14 Change absolute path to relative path

--- ci-compile.sh
+++ ci-compile.sh
@@ -33,7 +33,7 @@ arduino-cli compile --fqbn Seeeduino:samd:seeed_wio_terminal
 
 
 # Change directory to src folder of web app
-cd /builds/courses/dit113/2024/group-15/pll/Web/pll-web-app/src
+cd ../../Web/pll-web-app/src
 
 # Add mqtt credentials
 echo "export const HOST = '$WEB_MQTT_HOST';" > credentials.js
